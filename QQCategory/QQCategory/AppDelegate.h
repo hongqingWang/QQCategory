0a460a1e4ea06af26cdf9d268ce4728fc07d580e
hongqingWang <272338444@qq.com>
2018-02-01 21:32:45 +0800

    初始化代码
    初始化代码

---
+++ QQCategory/QQCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQCategory
+//
+//  Created by 王红庆 on 2018/2/1.
+//  Copyright © 2018年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQCategory/QQCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQCategory
+//
+//  Created by 王红庆 on 2018/2/1.
+//  Copyright © 2018年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
