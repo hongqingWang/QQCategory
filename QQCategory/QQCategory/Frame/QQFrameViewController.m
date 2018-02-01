9a032ce55e83b79f8faf8d02a4411dc0256fe031
hongqingWang <272338444@qq.com>
2018-02-01 22:14:39 +0800

    frame 测试代码完毕
    frame 测试代码完毕

--- QQCategory/QQCategory/Frame/QQFrameViewController.m
+++ QQCategory/QQCategory/Frame/QQFrameViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "QQFrameViewController.h"
+#import "UIView+QQ.h"
 
 @interface QQFrameViewController ()
 
@@ -26,6 +27,28 @@
     
     self.navigationItem.title = @"Frame";
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIView *view = [[UIView alloc] init];
+    view.qq_x = 100;
+    view.qq_y = 100;
+    view.qq_w = 200;
+    view.qq_h = 100;
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
+    NSLog(@"%@", view);
+    
+    UIView *view02 = [[UIView alloc] init];
+    /**
+     * 注意:
+     * 一定要先设置`size`
+     * 如果后设置`size`就会默认成`x = 200`、`y = 300`的效果
+     */
+    view02.qq_size = CGSizeMake(200, 100);
+    view02.qq_centerX = 200;
+    view02.qq_centerY = 300;
+    view02.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:view02];
+    NSLog(@"%@", view02);
 }
 
 @end
